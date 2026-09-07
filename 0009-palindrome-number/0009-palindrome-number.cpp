4db8ad269e7a7d3bc78da2902f2abe7e3d01cee3
DurgaReddy7747 <durgareddypadala7737@gmail.com>
2026-09-07 13:34:06 +0530

    Time: 4 ms (30.56%), Space: 8.6 MB (64.66%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        int y =x;
+        long long n=0;
+        while(x!=0){
+        n=n*10+(x%10);
+        x/=10;
+        }
+        if(n==y) return true;
+        else return false;
+    }
+};
